c850f9002af1be695e8ef636966910ddd54c6098
Young Zhang <young.quinn.zhang@gmail.com>
2013-05-30 01:53:32 -0400

    module 4, 05. Demo - Using the $http and $q Services Together / use $q.defer() in getEvent()

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -3,16 +3,23 @@
 eventsApp.controller('EventController',
     function EventController($scope, eventData) {
     	$scope.sortorder = 'name';
-        $scope.event = eventData.getEvent(function (event) {
-            $scope.event = event;
+        $scope.event = eventData.getEvent();
+        $scope.event.then(function(event) {
+            console.log(event);
+        },
+        function(status) {
+            console.log(status);
         });
-        // $scope.event = eventData.event;
 
-        $scope.upVoteSession = function(session) {
+        $scope.upVoteSession = function(session, event) {
+            console.log('event passed in by EventDetails.html:');
+            console.log(event);
+            console.log('$scope.event:');
+            console.log($scope.event);
             session.upVoteCount++;
         };
 
-        $scope.downVoteSession = function(session) {
+        $scope.downVoteSession = function(session, event) {
             session.upVoteCount--;
         };
     }
--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -1,16 +1,19 @@
 'use strict';
 
-eventsApp.factory('eventData', function($http, $log) {
+eventsApp.factory('eventData', function($http, $log, $q) {
 	return {
-        getEvent: function(onSuccess) {
+        getEvent: function() {
+            var deferred = $q.defer();
             $http({method: 'GET', url: '/data/event/1'}).
                 success(function(data, status, headers, config) {
                     $log.info(data, status, headers(), config);
-                    onSuccess(data);
+                    deferred.resolve(data);
                 }).
                 error(function(data, status, headers, config) {
                     $log.warn(data, status, headers(), config);
+                    deferred.reject(status);
                 });
+            return deferred.promise;
         }
 	}
 });
