797ef1c0d8c6a34fbba2a0f2511b703df8a3e5e8
Young Zhang <young.quinn.zhang@gmail.com>
2013-06-01 00:40:05 -0400

    M3/10. Demo - Using the $parse Service

--- app/js/controllers/CompileSampleController.js
+++ app/js/controllers/CompileSampleController.js
@@ -2,6 +2,22 @@
 
 eventsApp.controller('CompileSampleController',
     function CompileSampleController($scope, $compile, $parse) {
+        var fn = $parse('1 + 2');
+        console.log(fn());
+
+        var getter = $parse('event.name');
+
+        var context1 = { event: {name: 'AngularJS Boot Camp'}};
+        var context2 = { event: {name: 'Code Camp'}};
+
+        console.log(getter(context1));
+        console.log(getter(context2));
+        console.log(getter(context2, context1 /* local context */));
+
+        var setter = getter.assign;
+        setter(context2, 'Code Retreat');
+        console.log(context2.event.name);
+
         $scope.appendDivToElement = function(markup) {
             return $compile(markup)($scope).appendTo(angular.element("#appendHere"));
         }
