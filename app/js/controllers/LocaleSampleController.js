87bf269199b4427087e313c99742222bb72d7930
Young Zhang <young.quinn.zhang@gmail.com>
2013-06-01 00:45:44 -0400

    M3 / $locale service

---
+++ app/js/controllers/LocaleSampleController.js
@@ -0,0 +1,12 @@
+'use strict';
+
+eventsApp.controller('LocaleSampleController',
+    function LocaleSampleController($scope, $locale) {
+        console.log($locale);
+        $scope.myDate = Date.now();
+        $scope.myFormat = $locale.DATETIME_FORMATS.fullDate;
+
+        //throw { message: 'error message' };
+
+    }
+);
