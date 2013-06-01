3286d552c7f70415ea8050b82b5d49e2192023d0
Young Zhang <young.quinn.zhang@gmail.com>
2013-06-01 01:03:59 -0400

    M3 / $filter service - part 2, just inject filter via DI

--- app/js/controllers/FilterSampleController.js
+++ app/js/controllers/FilterSampleController.js
@@ -1,15 +1,13 @@
 'use strict';
 
 eventsApp.controller('FilterSampleController',
-    function FilterSampleController($scope, $filter) {
+    function FilterSampleController($scope, durationsFilter /* name of the filter followed by Filter */) {
         $scope.data = {};
 
-        var durations = $filter('durations');
-
-        $scope.data.duration1 = durations(1);
-        $scope.data.duration2 = durations(2);
-        $scope.data.duration3 = durations(3);
-        $scope.data.duration4 = durations(4);
+        $scope.data.duration1 = durationsFilter(1);
+        $scope.data.duration2 = durationsFilter(2);
+        $scope.data.duration3 = durationsFilter(3);
+        $scope.data.duration4 = durationsFilter(4);
 
     }
 );
